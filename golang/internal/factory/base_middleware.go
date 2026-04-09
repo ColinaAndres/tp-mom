61d765a32114554e0f1133f3d1db280ed0525547
Andres Colina <acolina@fi.uba.ar>
2026-04-08 23:12:43 -0300

    add: se crea metodo que encapsula loop de consumo

--- golang/internal/factory/base_middleware.go
+++ golang/internal/factory/base_middleware.go
@@ -3,6 +3,7 @@ package factory
 import (
 	"sync"
 
+	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -12,4 +13,34 @@ type baseMiddleware struct {
 	publisherChannel *amqp.Channel
 	consumerTag      string
 	consumingWaiting sync.WaitGroup
+}
+
+func (b *baseMiddleware) runConsumerLoop(msgs <-chan amqp.Delivery, callbackFunc func(msg m.Message, ack func(), nack func())) error {
+	b.consumingWaiting.Add(1)
+	defer b.consumingWaiting.Done()
+
+	var ackError error
+	for msg := range msgs {
+		callbackFunc(
+			m.Message{Body: string(msg.Body)},
+			func() {
+				if err := msg.Ack(false); err != nil {
+					ackError = m.ErrMessageMiddlewareMessage
+				}
+			},
+			func() {
+				if err := msg.Nack(false, true); err != nil {
+					ackError = m.ErrMessageMiddlewareMessage
+				}
+			},
+		)
+		if ackError != nil {
+			return ackError
+		}
+	}
+
+	if b.consumerChannel.IsClosed() {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+	return nil
 }
